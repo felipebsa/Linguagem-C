bdfc9a24a65b892fc7c0275f462bccc71194d8d1
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-01 13:51:27 -0300

    add 05 question

---
+++ C++/05ex.cpp
@@ -0,0 +1,18 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: 05ex.cpp
+* Data: 26/04/2026
+* Autor: Felipe Barbosa Santos
+* Descrição: 
+* Ler um número para a parada final e outro número que representa um múltiplo. 
+* Exibir os múltiplos do número lido de 1 até o número final lido.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
