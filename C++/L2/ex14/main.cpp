1c5de00d1c7809e8ff218811e788d3e38effca35
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 22:55:16 -0300

    feat: add ex14

--- C++/L2/ex14/main.cpp
+++ C++/L2/ex14/main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 int main() {
     int n = 10; 
-    vector<int> v(n);
+    vector<string> v(n);
     
     for (int i = 0; i < n; i++) cin >> v[i];
-    
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    for (int i=n-1; i >= 0; i--) {
+        cout << v[i] << endl;
+    }
     return 0;
 }
