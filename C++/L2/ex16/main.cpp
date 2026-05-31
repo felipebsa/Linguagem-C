80c07c265ecd8688f50d7926d3c8e593ceb8ad2d
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 23:25:31 -0300

    feat: add ex16

--- C++/L2/ex16/main.cpp
+++ C++/L2/ex16/main.cpp
@@ -5,10 +5,32 @@
 using namespace std;
 
 int main() {
-    int n = 10; 
+    int n = 15, more, less; 
     vector<int> v(n);
-    
-    for (int i = 0; i < n; i++) cin >> v[i];
-    
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        if(i == 0) {
+            more = v[i];
+            less = v[i];
+        }
+        if (more < v[i]) {
+            more = v[i];
+        }
+        if (less > v[i]) {
+            less = v[i];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (more == v[i]) {
+            cout << "big is indice [" << i << "]" << endl;
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (less == v[i]) {
+            cout << "small is indice [" << i << "]";
+            break;
+        }
+    }
     return 0;
 }
