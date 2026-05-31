e7a6b22e201226586d0210c7a47afe77409c9f78
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-31 10:52:32 -0300

    feat: add ex17

--- C++/L2/ex17/main.cpp
+++ C++/L2/ex17/main.cpp
@@ -5,10 +5,17 @@
 using namespace std;
 
 int main() {
-    int n = 10; 
-    vector<int> v(n);
-    
-    for (int i = 0; i < n; i++) cin >> v[i];
-    
+    int n = 10, sum1 = 0, sum2 = 0; 
+    vector<int> v1(n);
+    vector<int> v2(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v1[i];
+        v2[i] = v1[i] * -1;
+    }
+    for (int i = 0; i < n; i++) {
+        if (v1[i] > 0) sum1+=v1[i];
+        if (v2[i] > 0) sum2+=v2[i];
+    }
+    cout << sum1 + sum2 << endl;
     return 0;
 }
