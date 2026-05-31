f1d43ead4f869bac03b21ed6812c5fc0dfe83f37
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 21:26:25 -0300

    feat: add ex06

--- C++/L2/ex06/main.cpp
+++ C++/L2/ex06/main.cpp
@@ -4,7 +4,21 @@
 #include <algorithm>
 using namespace std;
 
+void week() {
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1: cout << "Segunda"; break;
+        case 2: cout << "Terça"; break;
+        case 3: cout << "Quarta"; break;
+        case 4: cout << "Quinta"; break;
+        case 5: cout << "Sexta"; break;
+        case 6: cout << "Sabado"; break;
+        case 7: cout << "Domingo"; break;
+        default: cout << "1-7";
+    };
+}
 int main() {
-    
+    week();
     return 0;
 }
